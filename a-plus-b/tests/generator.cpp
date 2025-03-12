e6fe9270bad214859fb3c26be2a6a3a5fcf6e918
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-03-12 18:38:05 +0900

    Generator を修正

--- a-plus-b/tests/generator.cpp
+++ a-plus-b/tests/generator.cpp
@@ -10,7 +10,6 @@ const int RANDOM_CASES = 10;
 int main(int argc, char* argv[])
 {
     registerGen(argc, argv, 1);
-    // random
     for(int i = 0; i < RANDOM_CASES; i++){
         int A = rnd.next(MIN_A, MAX_A);
         int B = rnd.next(MIN_B, MAX_B);
